66baacde227f246e6c3f678c366deddfbf27ac89
Arko Barman <116270662+don-arko@users.noreply.github.com>
2024-10-06 13:47:43 +0530

    Create goto.cpp

---
+++ CP_Notes/goto.cpp
@@ -0,0 +1,12 @@
+//USE CASE OF GOTO:- To break out of more than one nested loop we can use flag(for 2 nested loop) or goto comes in very handy
+
+for (){
+  for(){
+    for(){
+      goto endloops;
+    }
+  }
+}
+endloops;
+  cout<<ans;
+
