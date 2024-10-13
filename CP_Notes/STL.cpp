1706f47e90a14eb832f2f43c106d4b06783a94d3
Arko Barman <116270662+don-arko@users.noreply.github.com>
2024-10-13 19:40:37 +0530

    Update STL.cpp

--- CP_Notes/STL.cpp
+++ CP_Notes/STL.cpp
@@ -1,1 +1,16 @@
+
+
 pair<int,int> d[N]; //creates a array of pairs of ints  with all pairs initially set to (0,0)
+
+
+
+
+//MULTISET
+multiset<int,greater<int>> m;  //creates a multiset(duplicate allowed) and sorts in decreasing order(opposite of default asec sort)
+
+auto it=m.lower_bound(x);  //usage of lower bound. here in desc sorted array lower_bound gives <=
+
+
+
+
+
