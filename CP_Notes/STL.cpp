579f4d77fcf92a24a0472f522574dc814a74ce1f
Arko Barman <116270662+don-arko@users.noreply.github.com>
2024-10-19 17:39:24 +0530

    Update STL.cpp

--- CP_Notes/STL.cpp
+++ CP_Notes/STL.cpp
@@ -8,7 +8,19 @@ pair<int,int> d[N]; //creates a array of pairs of ints of size N  with all pairs
 -Usage of maps is like arrays think of a[i] where i is the key and a[i] is the value 
 -Most common usecase is frequency counting cause maps are flexible in term of range of indices.. it could be discrete
 where if we use arrays to count frequency then range of indices has to be almost continuous or else there is always wastage of space and it the constraint are high this wastage may not allow us to use arrays at all or limit errors may be raised.
+-[anything]++ creates the key "anything" if not present then increase frequency from 0 to 1 or else just updates the frequency if anything is present in the map.
+
 */
+
+//FEATURES OF MAP
+
+map<int,int> m;//creates a map of key and value both of type int.
+
+m[8]++;  //since the key 8 was not present in the map so it creates the key value {8,0} and then m[8]++ makes it {8,1};
+
+// - so we dont need to take care and can use m[anything]++ and anything gets created and its frequency is increase if anything not present or if present then its frequency is updated 
+// - But we can go on doing m[anything]-- after some point frequncy becomes negative but the key value pair still exists in the map so if we want to delete that we will have to handle the case frequency ==0 and erase the key-value pair manually.
+
 
 //-printing maps through iterators
 map<int,int> m;
